2ee7b2ad098e4f36444fb7d032f20b8d0e4b1b80
vicky1999 <ravivignesh2000@gmail.com>
2021-09-13 20:32:15 +0100

    shift cipher encryption implemented

--- src/crypto/ciphers.ts
+++ src/crypto/ciphers.ts
@@ -1,7 +1,7 @@
-import { CaesarCipher } from './caesar.cipher';
+import { ShiftCipher } from './shift.cipher';
 
 let ciphers = [
-    { name: 'Caesar Cipher', cipher: CaesarCipher }
+    { name: 'Shift Cipher', cipher: ShiftCipher }
 ];
 
 module.exports = ciphers;
---
+++ src/crypto/shift.cipher.ts
@@ -0,0 +1,12 @@
+
+export class ShiftCipher {
+    static encrypt = (text: string, amount: number): string => {
+        let res = '';
+        text = text.toUpperCase();
+        for(let i=0;i<text.length;i++) {
+            res+=String.fromCharCode(65+(text.charCodeAt(i) - 65 + amount)%26);
+        }
+
+        return res;  
+    };
+}
