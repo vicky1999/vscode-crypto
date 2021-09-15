abed6292239ae608292d117ca148eaf26430d0dd
vicky1999 <ravivignesh2000@gmail.com>
2021-09-16 00:32:57 +0530

    remove out folder

--- out/tests/extension/extension.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var assert = require('assert');
-describe('Initial Test', function () {
-    it('should be equal', function () {
-        assert.equal(1, 1);
-    });
-});
-//# sourceMappingURL=extension.test.js.map
