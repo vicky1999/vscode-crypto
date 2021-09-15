c9ba12380812eac00e87b0baa489aa681afc74ec
vicky1999 <ravivignesh2000@gmail.com>
2021-09-15 23:15:30 +0530

    Shift Cipher decryption issues corrected

--- src/crypto/shift.cipher.ts
+++ src/crypto/shift.cipher.ts
@@ -13,8 +13,13 @@ export class ShiftCipher {
     static decrypt = (text: string, amount: number) => {
         let res = '';
         text = text.toUpperCase();
+        let charCode = 0;
         for(let i=0;i<text.length;i++) {
-            res+=String.fromCharCode(65+(text.charCodeAt(i) - 65 - amount)%26);
+            charCode = (text.charCodeAt(i) - 65 - amount%26);
+            if(charCode < 0) {
+                charCode+=26;
+            }
+            res+=String.fromCharCode(65+charCode%26);
         }
         return res;
     };
