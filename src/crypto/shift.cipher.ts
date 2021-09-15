52e74b873e1861ff646fcc9582c0d434998a03ab
vicky1999 <ravivignesh2000@gmail.com>
2021-09-15 23:05:50 +0530

    Shift cipher decryption created

--- src/crypto/shift.cipher.ts
+++ src/crypto/shift.cipher.ts
@@ -9,4 +9,13 @@ export class ShiftCipher {
 
         return res;  
     };
+
+    static decrypt = (text: string, amount: number) => {
+        let res = '';
+        text = text.toUpperCase();
+        for(let i=0;i<text.length;i++) {
+            res+=String.fromCharCode(65+(text.charCodeAt(i) - 65 - amount)%26);
+        }
+        return res;
+    };
 }
