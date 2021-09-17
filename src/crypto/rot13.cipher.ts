df07a9100f68111609e2f9e280044930ee665168
vicky1999 <ravivignesh2000@gmail.com>
2021-09-17 22:14:29 +0530

    rot13 cipher encryption created

---
+++ src/crypto/rot13.cipher.ts
@@ -0,0 +1,11 @@
+
+export class Rot13 {
+    static encrypt(text: string) {
+        let res = '';
+        text = text.toUpperCase();
+        for(let i=0;i<text.length;i++) {
+            res+=String.fromCharCode(65 + (text.charCodeAt(i) - 65 + 13)%26);
+        }
+        return res;
+    }
+}
